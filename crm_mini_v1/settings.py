3d741c6ce9bfba5f56aea9fc3f8f87087b7197b8
Blackscure <34754398+Blackscure@users.noreply.github.com>
2025-01-02 22:46:39 +0300

    lead model created

--- crm_mini_v1/settings.py
+++ crm_mini_v1/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication.apps.AuthenticationConfig',
+    'lead.apps.LeadConfig',
 
      #third party
     'rest_framework',
