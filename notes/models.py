068263146e3cfeadd932ef3efab23611b4d27f92
Blackscure <34754398+Blackscure@users.noreply.github.com>
2025-01-03 03:18:43 +0300

    model created in notes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from lead.models import Lead
+
+
+class Note(models.Model):
+    lead = models.ForeignKey(Lead, related_name='notes', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
