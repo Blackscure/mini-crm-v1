9cc2a2b3f0570dd74adea186c71396ee67f773b6
Blackscure <34754398+Blackscure@users.noreply.github.com>
2025-01-03 03:01:59 +0300

    ft-contact

--- contact/api/serializers.py
+++ contact/api/serializers.py
@@ -1,11 +1,21 @@
 from contact.models import Contact
 from lead.api.serializers import LeadSerializer
+from lead.models import Lead
 from rest_framework import serializers
 
 
 
 class ContactSerializer(serializers.ModelSerializer):
-    lead = LeadSerializer() 
+    lead = serializers.PrimaryKeyRelatedField(queryset=Lead.objects.all())
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['lead'] = {
+            'id': instance.lead.id,
+            'name': instance.lead.name
+        }
+        return data
+
     class Meta:
         model = Contact
         fields = ['id', 'lead', 'name', 'email', 'phone']
